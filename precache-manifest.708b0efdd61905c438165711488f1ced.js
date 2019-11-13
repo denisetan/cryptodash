df1e74b5f92664dd3caebdc4e9106f80b260bc7e
Denise Tan <techdenisetan@gmail.com>
2019-11-13 22:55:30 +0800

    Updates

---
+++ precache-manifest.708b0efdd61905c438165711488f1ced.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4485524324d542c8a762ee12c4ba0e7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9934553b81a79f5f08c0",
+    "url": "/static/css/main.d8a130f5.chunk.css"
+  },
+  {
+    "revision": "74638a44321f224c347c",
+    "url": "/static/js/2.0db63a9f.chunk.js"
+  },
+  {
+    "revision": "9934553b81a79f5f08c0",
+    "url": "/static/js/main.713aeb45.chunk.js"
+  },
+  {
+    "revision": "a15425a2e8e7b5e37c83",
+    "url": "/static/js/runtime~main.c788eff8.js"
+  }
+]);
